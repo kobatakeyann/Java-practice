5c93c3ce2e913ba312e3390a3e01f55e3c2baf32
kobatakeyann <suu_hppa@outlook.jp>
2025-02-11 18:28:44 +0900

    first commit

---
+++ chapter_4/exercise/1/ExplainCorporate.java
@@ -0,0 +1,9 @@
+class ExplainCorporate {
+    public static void main(String[] args) {
+        String message = """
+                シンプレクスは1997年に創業されました。
+                コーポレートスローガンは "Hello world, Hello innovation." です。
+                """.stripTrailing();
+        System.out.println(message);
+    }
+}
